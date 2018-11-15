42736e4e6265969730e591df6f840ab5c326fca0
troyp <troyprag816@gmail.com>
2018-11-14 23:32:31 -0500

    add gravition area shader

---
+++ Assets/Shader/ShockwaveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockwaveScript : MonoBehaviour {
+
+    Renderer rend;
+    float magnitude;
+    public float shifting_speed = 0.05f;
+    void Start () {
+        rend = GetComponent<Renderer>();
+
+        // Use the Specular shader on the material
+        rend.material.shader = Shader.Find("Effects/WaveEffect");
+    }
+	
+	
+	void Update ()
+    {
+        magnitude += shifting_speed;
+        if (magnitude >= 5)
+            magnitude = 0;
+        rend.material.SetFloat("_Magnitude", magnitude);
+    }
+}
+    
